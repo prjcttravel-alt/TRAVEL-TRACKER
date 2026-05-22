47088f59d72ed600181faa0f1f5f5806a7d342c4
rahulramesh97 <rahulgullu1997@gmail.com>
2026-05-22 15:14:13 +0530

    Reconnect MongoDB

--- server/config/db.js
+++ server/config/db.js
@@ -4,8 +4,7 @@ mongoose.set('bufferTimeoutMS', 60000);
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI;
-    if (!uri) throw new Error('MONGO_URI is not defined');
+    const uri = process.env.MONGO_URI || 'mongodb+srv://prjcttravel_db_user:Pokemon47@cluster0.zrrkpq9.mongodb.net/?appName=Cluster0';
     const conn = await mongoose.connect(uri, {
       serverSelectionTimeoutMS: 3000,
       connectTimeoutMS: 3000,
