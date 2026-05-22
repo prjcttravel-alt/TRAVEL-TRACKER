d104a065d82e2a26b3ac53b3263fce5f11501731
rahulramesh97 <rahulgullu1997@gmail.com>
2026-05-22 15:02:39 +0530

    Fix GitHub pages base path and set API URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.RENDER ? '/' : '/travel-agency/',
+  base: process.env.RENDER ? '/' : '/TRAVEL-TRACKER/',
   plugins: [react()],
   server: {
     proxy: {
